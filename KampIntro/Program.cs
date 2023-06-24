45f4eda5a2f63ae829a853da75c8b13ef3681463
Mustafa Gülaç <gulacmustafa1@gmail.com>
2023-06-24 18:22:35 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace KampIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //Do not repeat yourself - Kendini tekrarlama
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.55;
+
+            if(dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış butonu");
+            }
+            else if(dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artış butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayaları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+
+            
+            
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+     class SepetManager
+    {
+        //naming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(urun.Adi + " Sepete Eklendi");
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine(urunAdi + " Sepete Eklendi");
+        }
+    }
+}
