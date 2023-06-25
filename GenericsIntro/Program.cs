7bd41dcd1821b0725a006191a1b9ca63c0c95369
Mustafa Gülaç <gulacmustafa1@gmail.com>
2023-06-25 14:43:22 +0300

    added GenericsIntro, Koleksiyonlar,OOP1 projects

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Mustafa");
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Muhammed");
+            Console.WriteLine(isimler.Length);
+
+            foreach (var item in isimler.Items) {
+
+                Console.WriteLine(item); 
+            
+            }
+            
+        }
+    }
+}
