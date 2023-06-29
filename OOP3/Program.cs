aa0333ef3e886c9bfcd6315089c33ccdc7cf359b
Mustafa Gülaç <gulacmustafa1@gmail.com>
2023-06-29 13:00:46 +0300

    added OOP2 and OOP3 projects

---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            List<ILoggerService> loggerServices = new List<ILoggerService> { databaseLoggerService,smsLoggerService};
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(esnafKrediManager, loggerServices ); 
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager,tasitKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
